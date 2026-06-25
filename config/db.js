b78c1eb2624c7b77bf717fc5bc8808a18b3f40aa
komala28 <komalisiri96@gmail.com>
2026-06-25 16:50:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const mysql = require("mysql2");
 
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("DB:", process.env.DB_NAME);
+console.log("HOST:", process.env.MYSQL_HOST);
+console.log("USER:", process.env.MYSQL_USER);
+console.log("DB:", process.env.MYSQL_DATABASE);
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 connection.connect((err) => {
